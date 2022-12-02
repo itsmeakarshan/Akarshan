26780d968eae56c2598d9289ac50012da9995a1b
itsmeakarshan <93965040+itsmeakarshan@users.noreply.github.com>
2022-12-02 10:14:25 +0530

    Update Prac6.py

--- Practical_1/Prac6.py
+++ Practical_1/Prac6.py
@@ -2,8 +2,8 @@
 
 a=int(input("Enter value of a: "))
 b=int(input("Enter value of b: "))
-print("Before Swapping:")
-print("a =",a,"b =",b)
+print("Before Swapping:\n" "a =",a,"b =",b)
+
 a,b=b,a
 print("After swapping:")
 print("a =",a,"b =",b)
